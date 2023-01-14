6619de6d5f5b75738fc847434c0f37cbdf1fdd91
Rodrigo Bernal <Rodrigo.Bernal@jala.university>
2023-01-13 22:06:45 -0400

    Add Scenario Outline for negative testing of login

---
+++ app/src/test/java/stepDefinitions/LoginSteps.java
@@ -0,0 +1,75 @@
+package StepDefinitions;
+
+import org.aeonbits.owner.ConfigFactory;
+import org.junit.Assert;
+
+import api.login.Login;
+import api.properties.ConfigProp;
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import io.restassured.response.Response;
+
+public class LoginSteps {
+
+    Response response;
+
+
+    @Given("The user has a valid account")
+    public void validAccount() {
+        System.out.println("The user has a valid account");
+    }
+
+    @When("The user login into the page")
+    public void loginIntoThePage() {
+        Login login = new Login();
+        ConfigProp config = ConfigFactory.create(ConfigProp.class);
+        response = login.login(config.username(), config.password());
+        System.out.println("The is logging into the page...");
+    }
+
+    @Then("The user session is opened")
+    public void sessionOpened() {
+        Assert.assertEquals(664426, response.jsonPath().getInt("id"));
+        Assert.assertEquals("rodrigo.bernal", response.jsonPath().
+                getString("username"));
+        Assert.assertEquals("Rodrigo", response.jsonPath().getString("first_name"));
+        Assert.assertEquals("Bernal", response.jsonPath().getString("last_name"));
+        Assert.assertEquals("rodrigo.bernal@jala.university", response.jsonPath()
+                .getString("email"));
+        Assert.assertEquals("2022-12-21T15:02:16.417500Z", response.jsonPath()
+                .getString("date_joined"));
+        System.out.println("The user session is opened");
+    }
+
+    @And("A Status Code OK is displayed")
+    public void statusCodeOk() {
+        Assert.assertEquals(200, response.getStatusCode());
+    }
+
+    @Given("The user does not have a valid account")
+    public void theUserDoesNotHaveAValidAccount() {
+        System.out.println("The user does not have a valid account");
+    }
+
+    @When("The user enters {string} and {string}")
+    public void theUserEntersUsernameAndPassword(String username, String password) {
+        Login login = new Login();
+        response = login.login(username, password);
+        System.out.println("The user is trying to login into the page");
+    }
+
+    @Then("The user session is not opened")
+    public void theUserSessionIsNotOpened() {
+        Assert.assertEquals("[Incorrect username or password.]",
+                response.jsonPath().getString("non_field_errors"));
+        System.out.println("The user cannot login into the page");
+    }
+
+    @And("A Status Code Bad Request is displayed")
+    public void aStatusCodeUnahutorizationIsDisplayed() {
+        Assert.assertEquals(400, response.getStatusCode());
+    }
+
+}
